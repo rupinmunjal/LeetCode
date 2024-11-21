d1e7188b87713e8ff6803a3e134126dcc7865ea0
Rupin Munjal <rupinmunjal05@gmail.com>
2024-11-20 19:11:41 -0500

    removed everything

--- 26_Remove_Duplicates_from_Sorted_Array.py
+++
@@ -1,7 +0,0 @@
-def removeDuplicates(nums):
-        j = 0
-        for i in range(len(nums)):
-            if nums[i] != nums[j]:
-                j += 1
-                nums[j] = nums[i]
-        return j + 1
